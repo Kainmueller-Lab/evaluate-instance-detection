5bb5924a4448773a06921df0f19a10ad542a295d
Peter Hirsch <abred@users.noreply.github.com>
2019-09-02 14:15:33 +0200

    init

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+        name='evaluate-instance-detection',
+        version='0.1',
+        description='Evaluate instance detection.',
+        url='',
+        author='Peter Hirsch',
+        author_email='kainmuellerlab@mdc-berlin.de',
+        license='MIT',
+        packages=[
+                'evaluateInstanceDetection',
+        ]
+)
